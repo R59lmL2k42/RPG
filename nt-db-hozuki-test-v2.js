d821335b9f8b0893a45b327eeebe29e64207a754
R59lmL2k42 <tenseirpg@gmail.com>
2026-06-01 11:44:13 +0300

    Create nt-db-hozuki-test-v2.js

---
+++ nt-db-hozuki-test-v2.js
@@ -0,0 +1,26 @@
+console.log("NT Hozuki test v2 cargado");
+
+NT_addJutsus({
+ "test_hozuki": {
+  "name": "Test Hozuki",
+  "translation": "",
+  "art": "Ninjutsu",
+  "type": "Instantánea",
+  "rank": "E",
+  "icon": "水",
+  "color": "#4DA7C7",
+  "cost": "1 Chakra",
+  "attrs": "CCH 5",
+  "availability": "Libre",
+  "clanSchool": "Hōzuki · Hiden",
+  "damage": "Sin daño",
+  "nature": "Suiton (Agua)",
+  "yy": "No",
+  "seals": "Sin sellos",
+  "range": "0 metros",
+  "area": "Objetivo individual",
+  "cooldown": "0 turnos",
+  "effects": "Prueba externa.",
+  "description": "Prueba mínima de carga externa."
+ }
+});
